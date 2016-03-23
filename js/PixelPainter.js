cbf726cd24756392c76ddc1f2e454d0eb1c9188b
Stephen <stephenlallum@gmail.com>
2016-03-22 20:05:55 -1000

    latest commit

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,4 +1,19 @@
-window.onload  = function painterCanvas(width, height) {
+window.onload = function painterCanvas(width, height) {
+
+
+    var canvasColors = ['#ff0000', '#ff4000', '#ffbf00', '#ffff00', '#80ff00', '#40ff00', '#00ff00', '#00ff40', '#00ff80', '#00ffbf', '#00ffff'];
+    //setting boxes to be able to be colored,
+    function colorSetter() {
+        var divColors = document.getElementById('color-palette');
+        for (var i = 0; i < canvasColors.length; i++) {
+            var newDiv = document.createElement('div');
+            newDiv.classList.add('colors');
+            divColors.appendChild(newDiv);
+            newDiv.style.backgroundColor = canvasColors[i];
+
+        }
+    }
+    colorSetter();
 
 
 
@@ -7,24 +22,53 @@ window.onload  = function painterCanvas(width, height) {
     var boxes = document.querySelectorAll('#pp-canvas .box');
     console.log(boxes);
     for (var i = 0; i < boxes.length; i++) {
+        var colored = false;
         boxes[i].addEventListener('click', function(event) {
             this.style.backgroundColor = 'red';
+            colored = true;
         });
-        
-        //console.log(boxes);
+    };
+
+    // to select color
+
+    function applyColor() {
+
+        var colorPalette = document.getElementById('#color-palette');
+        var colorBoxes = document.querySelectorAll('.box');
+        for (var i = 0; i < colorBoxes.length; i++) {
+            colorBoxes[i].addEventListener('click', function(event) {
+            		this.style.backgroundColor = savedColor;
+                });
+
+        }
     }
+applyColor();
 
 
 
+//to select new color
+    var savedColor;
+
+    function selectNewColor() {
+        var colorPalette = document.getElementById('#color-palette');
+        var colorBoxes = document.querySelectorAll('.colors');
+        //console.log(colorBoxes);
+        for (var i = 0; i < colorBoxes.length; i++) {
+            //console.log(colorBoxes[i].style);
+            colorBoxes[i].addEventListener('click', function(event) {
+                savedColor = this.style.backgroundColor;
+                console.log(savedColor);
+            });
 
 
-    function selectPixel() {
 
-
-
-
-
+        }
     }
+    selectNewColor();
+
+
+    
+
 
 
     function erasePixel() {
@@ -46,7 +90,7 @@ window.onload  = function painterCanvas(width, height) {
 
     return {
 
-        
+
         selectPixel: selectPixel,
         erasePixel: erasePixel,
         clearPixels: clearPixels
@@ -54,6 +98,7 @@ window.onload  = function painterCanvas(width, height) {
 
 
     };
+
 
 
 };
